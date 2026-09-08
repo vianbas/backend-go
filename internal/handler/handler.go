c3b16c04d41eb34ce61939700ffcb69703ab6c7d
vikoabastian <11003051+vianbas@users.noreply.github.com>
2026-09-08 16:40:46 +0700

    demo: greeting take 16:40:46

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -63,7 +63,7 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 
 // fetchGreetingFromDB mensimulasikan query DB
 func fetchGreetingFromDB() string {
-	return "Halo dari vianbas — take 16:17:13"
+	return "Halo dari vianbas — take 16:40:46"
 }
 
 // callExternalVersionAPI mensimulasikan panggilan ke external HTTP API
